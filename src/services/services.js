fea868e415dda004daa61315df0c0f59c942322d
Muzeffer <muzeffer9b2004@gmail.com>
2025-06-15 18:07:59 +0400

    API bug solved

--- src/services/services.js
+++ src/services/services.js
@@ -49,7 +49,10 @@ export const getCountryDataFromCode = async (code) => {
 
 export const getAllCountryNames = async () => {
   try {
-    const response = await fetch("https://restcountries.com/v3.1/all");
+    const response = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,cca2,continents"
+    );
+
     if (!response.ok) {
       throw new Error("Failed to fetch country list.");
     }
